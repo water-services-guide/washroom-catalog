da52a0d53cf4b066101429a6f8f6b8e1f4a374d4
Alex <asvatukhin@gmail.com>
2019-06-06 12:10:18 -0700

    Implement UserList and NecessityList

--- backend/app.py
+++ backend/app.py
@@ -18,17 +18,42 @@ mysql.init_app(app)
 def getUsers():
     connect = mysql.connect()
     cursor = connect.cursor()
-    cursor.execute('SELECT name, passwrd FROM User')
-    results = [{name: passwrd} for (name, passwrd) in cursor]
+    cursor.execute('SELECT User_id, Username, Password FROM User')
+    results = [{'User_id': User_id,
+                'Username': Username,
+                'Password': Password} for (User_id, Username, Password,) in cursor]
     cursor.close()
     connect.close()
 
     return results
 
 
+def getNecessities():
+    connect = mysql.connect()
+    cursor = connect.cursor()
+    cursor.execute('SELECT Necessity_id, Status, Building_id FROM Necessity')
+    results = [{'Necessity_id': Necessity_id,
+                'Status': Status,
+                'Building_id': Building_id} for (Necessity_id, Status, Building_id) in cursor]
+    cursor.close()
+    connect.close()
+
+    return results
+
+
+@app.route('/UserList')
+def userList():
+    return json.dumps(getUsers())
+
+
+@app.route('/NecessityList')
+def necessityList():
+    return json.dumps(getNecessities())
+
+
 @app.route('/')
-def index():
-    return json.dumps({'Users': getUsers()})
+def main():
+    return 'Welcome to Washroom Catalog'
 
 
 if __name__ == '__main__':
