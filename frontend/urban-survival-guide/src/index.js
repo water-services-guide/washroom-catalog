c00dcc1a1b0ef2b8233dbad6085e0878c3e3e49f
annnnnieee <anastasiya.vt.s@gmail.com>
2019-06-08 18:07:13 -0700

    added react router

--- frontend/urban-survival-guide/src/index.js
+++ frontend/urban-survival-guide/src/index.js
@@ -2,8 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import { Route, BrowserRouter as Router } from 'react-router-dom'
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+const routing = (
+  <Router>
+    <div>
+      <Route path="/" component={App} />
+    </div>
+  </Router>
+)
+
+ReactDOM.render( routing, document.getElementById('root') );
