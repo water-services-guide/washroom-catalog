4796bb23ee7cc48b541b45dded8517d6b9abacce
annnnnieee <anastasiya.vt.s@gmail.com>
2019-06-14 10:13:01 -0700

    installed semantic ui for react

--- frontend/urban-survival-guide/src/index.js
+++ frontend/urban-survival-guide/src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import { Route, BrowserRouter as Router } from 'react-router-dom'
+import NecessityDetail from './components/necessity-detail/necessity-detail';
+import 'semantic-ui-css/semantic.min.css'
 
 const routing = (
   <Router>
     <div>
-      <Route path="/" component={App} />
+      <Route exact path="/necessity/id" component={NecessityDetail} />
+      <Route exact path="/" component={App} />
     </div>
   </Router>
 )
-
-ReactDOM.render( routing, document.getElementById('root') );
+ ReactDOM.render( routing, document.getElementById('root') );
