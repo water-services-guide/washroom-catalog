8ab7daaed5f5c9647f6559ffb957ffe43051031f
annnnnieee <anastasiya.vt.s@gmail.com>
2019-06-14 19:48:39 -0700

    added sql for necessity details

--- backend/washroomcatalog/lists.py
+++ backend/washroomcatalog/lists.py
@@ -1,7 +1,7 @@
 import json
-
+from flask import jsonify
 from washroomcatalog import mysql
-
+import datetime
 
 def getObjects(table, attributes):
     connect = mysql.connect()
@@ -26,3 +26,73 @@ def getUsers():
 
 def getNecessities():
     return getObjects('Necessity', ['Necessity_id', 'Status', 'Building_id'])
+
+def getComments(necessity_id):
+    return execute("""
+    SELECT Username, Comment 
+    FROM User u, Comment c 
+    WHERE u.User_id = c.User_id 
+    AND Necessity_id = {necessity_id}
+    """.format(necessity_id=necessity_id))
+
+def getBuildingDetails(necessity_id):
+    return execute("""
+    SELECT * 
+    from Building b, PostalCode po, Necessity n, Favourite f
+    where b.Postal_code = po.Postal_code
+    AND f.Building_id = b.Building_id
+    AND n.Building_id = b.Building_id
+    AND n.Necessity_id = {necessity_id}
+    """.format(necessity_id=necessity_id))
+
+def getFavouriteBuilding(user_id, necessity_id):
+    return execute("""
+    SELECT *
+    from Necessity n, Favourite f
+    WHERE n.Necessity_id = {necessity_id}
+    AND n.Building_id = f.Building_id
+    AND f.user_id =  {user_id}
+    """.format(necessity_id=necessity_id, user_id=user_id))
+
+def getMaintenanceCompanyInfo(necessity_id):
+    return execute("""
+    SELECT * 
+    from MaintainenceCompany m, MaintainedBy mb
+    WHERE m.Company_id = mb.Company_id
+    AND mb.Necessity_id = {necessity_id}
+    """.format(necessity_id=necessity_id))
+
+def getWashroomDetails(necessity_id):
+    return execute("""
+    SELECT * 
+    from Necessity n, Washroom w
+    Where n.Necessity_id=w.Necessity_id
+    and n.Necessity_id = {necessity_id}
+    """.format(necessity_id=necessity_id))
+
+def getWaterFountainDetails(necessity_id):
+    return execute("""
+    Select * 
+    from Necessity n, WaterFountain w
+    where n.Necessity_id = w.Necessity_id
+    and n.Necessity_id = {necessity_id}
+    """.format(necessity_id=necessity_id))
+
+def getShowerDetails(necessity_id):
+    return execute("""
+    Select * 
+    from Necessity n, Shower s
+    where n.Necessity_id = s.Necessity_id
+    and n.Necessity_id = {necessity_id}
+    """.format(necessity_id=necessity_id))
+
+def execute(query):
+    connection = mysql.connect()
+    cursor = connection.cursor()
+    cursor.execute(query)   
+    result = cursor.fetchall()
+    cursor.close()
+    connection.close()
+    return result
+
+
