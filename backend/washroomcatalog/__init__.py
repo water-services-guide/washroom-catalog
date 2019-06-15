1ea8ef9f9fe43b064af5530c48835c0134bfda6a
annnnnieee <anastasiya.vt.s@gmail.com>
2019-06-14 19:48:06 -0700

    added cors support

--- backend/washroomcatalog/__init__.py
+++ backend/washroomcatalog/__init__.py
@@ -1,10 +1,13 @@
 from washroomcatalog.config import configure
 from flask import Flask
 from flaskext.mysql import MySQL
+from flask_cors import CORS
+from pymysql.cursors import DictCursor
 
 app = Flask(__name__)
-mysql = MySQL()
+mysql = MySQL(cursorclass=DictCursor)
 configure(app)
 mysql.init_app(app)
+CORS(app)
 
 from washroomcatalog import routes
